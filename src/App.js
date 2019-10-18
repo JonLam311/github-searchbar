3f98bd19677ce2d7a1b705e595a7f79d23fd4e6a
Guillaume Bellamy <guillaume_bellamy@outlook.com>
2019-10-18 15:05:06 +0200

    feat: add fetching users and comment list on specific issue

--- src/App.js
+++ src/App.js
@@ -1,15 +1,66 @@
-import React from 'react';
-import SearchBar from './Components/SearchBar';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import SearchBar from "./Components/SearchBar";
+import "./App.css";
+import UserList from "./Components/UserList";
 
 function App() {
-  const onSubmit = (value) => {
-    console.log('onSubmit fired', value)
-  }
+  const [issueUrl, setIssueUrl] = useState("");
+  const [issue, setIssue] = useState(null);
+  const [comments, setComments] = useState([]);
+  const [users, setUsers] = useState(null);
+
+  console.log(issue, comments, users);
+
+  const onSubmit = value => {
+    setIssueUrl(value);
+  };
+
+  useEffect(() => {
+    const doFetch = async () => {
+      try {
+        const response = await fetch(
+          `https://api.github.com/repos/${issueUrl}`
+        );
+        setIssue(await response.json());
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    if (issueUrl) {
+      doFetch();
+    }
+  }, [issueUrl]);
+
+  useEffect(() => {
+    const getComments = async () => {
+      try {
+        const response = await fetch(issue.comments_url);
+        setComments(await response.json());
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    if (issue) {
+      getComments();
+    }
+  }, [issue]);
+
+  useEffect(() => {
+    const users = comments.reduce((acc, comment) => {
+      if (acc.some(user => comment.user.id === user.id)) {
+        return acc;
+      }
+      return [...acc, comment.user];
+    }, []);
+    if (comments.length) {
+      setUsers(users);
+    }
+  }, [comments]);
 
   return (
     <div className="App">
       <SearchBar onSubmit={onSubmit} />
+      <UserList users={[]} />
     </div>
   );
 }
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,21 +1,27 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 
 const SearchBar = ({ onSubmit }) => {
-    const [value, setValue] = useState('')
-    const onChange = e => setValue(e.target.value)
-    const onClick = () => onSubmit(value)
-    const myInput = useRef()
-    
-    useEffect(() => {
-        myInput.current.focus()
-    }, [])
-    
+  const [value, setValue] = useState("nodejs/node/issues/6867");
+  const onChange = e => setValue(e.target.value);
+  const onClick = () => onSubmit(value);
+  const myInput = useRef();
+
+  useEffect(() => {
+    myInput.current.focus();
+  }, []);
+
   return (
     <div className="SearchBar">
-      <input type="text" name="search" onChange={onChange} value={value} ref={myInput} />
+      <input
+        type="text"
+        name="search"
+        onChange={onChange}
+        value={value}
+        ref={myInput}
+      />
       <button onClick={onClick}>SEARCH</button>
     </div>
   );
-}
+};
 
 export default SearchBar;
